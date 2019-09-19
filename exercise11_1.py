f1d4775bda8c07cf459a2de7a0181378f0578dd0
T.  Ramjee <tramjee@gmail.com>
2019-09-18 20:21:50 -0500

    test

---
+++ exercise11_1.py
@@ -0,0 +1,19 @@
+import re
+fileToOpen=input('Enter the file to Open')
+fd=open(fileToOpen)
+numlist = list()
+for line in fd:
+   line = line.rstrip()
+
+   y = re.findall('[0-9]+',line)
+   if y != []:
+       numlist.extend(y)
+
+total = 0
+for x in numlist:
+    total = total + int(x)
+
+print(total)
+
+
+
